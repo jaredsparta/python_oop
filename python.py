7cf25203e566bfb4bc7dd5fd324a0bc652ac4bb3
jared <jtlss14@gmail.com>
2020-11-05 11:49:27 +0000

    commented out test code

--- python.py
+++ python.py
@@ -26,23 +26,23 @@ class Python(Snake):
 
 python = Python()
 
-# Methods and attributes from Python class
-print("From Python:")
-print(python.shed_skin())
-print(python.size)
-
-# From Snake class
-print("\nFrom Snake:")
-print(python.limbs)
-print(python.use_tongue_to_smell())
-
-
-# From Reptile class
-print("\nFrom Reptile:")
-print(python.use_venom())
-print(python.tetrapod)
-
-# From Animal class
-print("\nFrom Animal:")
-print(python.alive)
-print(python.procreate())
+# # Methods and attributes from Python class
+# print("From Python:")
+# print(python.shed_skin())
+# print(python.size)
+# 
+# # From Snake class
+# print("\nFrom Snake:")
+# print(python.limbs)
+# print(python.use_tongue_to_smell())
+# 
+# 
+# # From Reptile class
+# print("\nFrom Reptile:")
+# print(python.use_venom())
+# print(python.tetrapod)
+# 
+# # From Animal class
+# print("\nFrom Animal:")
+# print(python.alive)
+# print(python.procreate())
